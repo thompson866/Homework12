49127cf97f880b2a0dffc766ad1469fdc5435550
arxip <arxipov1989@gmail.com>
2023-02-08 23:54:02 +0300

    домашка

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private  String nameBook;
     private Author author;
@@ -40,19 +42,17 @@ public class Book {
     public String toString() {
     return nameBook + author+ year;
     }
+
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-        Book book = (Book) obj;
-        return year == book.year && nameBook.equals(book.getNameBook()) && author.equals(book.getAuthor());
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(nameBook, book.nameBook) && Objects.equals(author, book.author);
     }
+
     @Override
-    public int hashCode(){
-        return 1;
+    public int hashCode() {
+        return Objects.hash(nameBook, author, year);
     }
 }
